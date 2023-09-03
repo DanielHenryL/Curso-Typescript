38d42c907a0496a47898115e36e0b2c62a642085
DanielHenryL <lagunasdaniel001@gmail.com>
2023-09-03 11:19:07 -0500

    Metodos publicos y private

--- 01-bases/classes/basica.ts
+++ 01-bases/classes/basica.ts
@@ -5,16 +5,23 @@
         // public realName?:string;
         // propiedad estatica => se puede consumir de manera global haciendo referencia a la misma clase(no a la instancia de la clase)
         static avgAge: number = 35;
-
+        static getAvgAge(){
+            return this.name // este ame es literalmente el nombre de la clase 'Avenger'
+        }
         constructor( 
-            private name:string, 
+            private name:string,    
             private team:string, 
             public realName?:string
         ) {}
+        // por defecto todo metodo es public, ni siquiera poniendole private bio().
+        public bio(){
+            return `${ this.name } (${this.team})`
+        }
     }
 
     const antman:Avenger = new Avenger('Antman','Capitan','Scott Lang');
     // console.log( antman );
-    console.log( antman );
+    // console.log( antman.bio() );
+    console.log( Avenger.getAvgAge());
 
 })()
