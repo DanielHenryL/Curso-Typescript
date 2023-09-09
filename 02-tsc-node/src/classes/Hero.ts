b9b54a00a154bf6d0cf47316cbefce4948e43f0f
DanielHenryL <lagunasdaniel001@gmail.com>
2023-09-09 17:09:04 -0500

    resolucion de errores, cuando usar "? y el !"

--- 02-tsc-node/src/classes/Hero.ts
+++ 02-tsc-node/src/classes/Hero.ts
@@ -1,11 +1,18 @@
+import powers from "../data/powers";
 
 export class Hero {
     public name:string;
-    public realName:string;
+    public powerId:number;
     public age:number
-    constructor( name:string, realName:string, age:number){
+    constructor( name:string, powerId:number, age:number){
         this.name = name;
-        this.realName = realName;
+        this.powerId = powerId;
         this.age = age;
+    }
+
+    get power():string{
+        // el '?' siginica 'SI el valor existe has lo siguiente'
+        // el '!' siginifica 'Estoy seguro que vendra el valor'
+        return powers.find( power => power.id === this.powerId )?.name || 'Not found';
     }
 }
--- 02-tsc-node/src/index.ts
+++ 02-tsc-node/src/index.ts
@@ -1,10 +1,11 @@
 import * as h from "./classes/Hero";
-import powers from "./data/powers";
+import powers, { Power } from "./data/powers";
 // esportacion con alias
 
-const heroe = new h.Hero('daniel','Henry',26);
+const heroe = new h.Hero('daniel',2,26);
 
-console.log( powers );
+console.log( heroe );
+console.log( heroe.power );
 // console.log( heroe );
 
 
